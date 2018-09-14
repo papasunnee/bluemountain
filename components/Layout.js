16bd3cd6d1ae75fc52516786c3040a7e258dc123
Adetona Sunday <papasunnee@gmail.com>
2018-09-14 16:19:23 +0100

    pusk

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,25 @@
 import React, { Component, Fragment } from "react";
-import { Container, Row, Col, Button } from "reactstrap";
-import Nav from "../components/Header/Nav";
+import Router from "next/router";
+import Head from "next/head";
+import { Container } from "reactstrap";
 import Footer from "../components/Footer/Footer";
+import NProgress from "nprogress";
+
+// progress bar
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 export default class extends Component {
   state = {};
   render() {
     return (
       <Fragment>
+        <Head>
+          <link rel="stylesheet" href="/static/css/nprogress.css" />
+        </Head>
         <Container
           fluid
           style={{
