f6c8f2f598866a5b944f2c642207bcb2a837d924
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-24 14:46:23 +0100

    Update app

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,51 +1,46 @@
-import React from 'react';
-import App, { Container } from 'next/app';
-import { MuiThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import JssProvider from 'react-jss/lib/JssProvider';
-import getPageContext from '../lib/getPageContext';
+/* eslint-disable no-undef */
+import React from 'react'
+import App, { Container } from 'next/app'
+import ApolloClient from "apollo-boost"
+import { ApolloProvider } from "react-apollo"
+import fetch from 'isomorphic-unfetch'
+// import Router from 'next/router'
+// import { initGA, logPageView } from '../utils/analytics'
 
-class MyApp extends App {
-  constructor(props) {
-    super(props);
-    this.pageContext = getPageContext();
-  }
-
-  pageContext = null;
-
-  componentDidMount() {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <Container>
-        {/* Wrap every page in Jss and Theme providers */}
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          {/* MuiThemeProvider makes the theme available down the React
-              tree thanks to React context. */}
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
-          >
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            {/* Pass pageContext to the _document though the renderPage enhancer
-                to render collected styles on server side. */}
-            <Component pageContext={this.pageContext} {...pageProps} />
-          </MuiThemeProvider>
-        </JssProvider>
-      </Container>
-    );
-  }
+if (!process.browser) {
+  global.fetch = fetch
 }
 
-export default MyApp;
+const client = new ApolloClient({
+  uri: process.env.BACKEND_URL || '/graphql'
+});
+
+export default class MyApp extends App {
+  static async getInitialProps ({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  componentDidMount () {
+    // initGA()
+    // logPageView()
+    // Router.router.events.on('routeChangeComplete', logPageView)
+  }
+
+  render () {
+    // console.log(process.env.BACKEND_URL);
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <ApolloProvider client={client}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </Container>
+    )
+  }
+}
