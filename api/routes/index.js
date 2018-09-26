2aa9eec1944e6544596b9f93d2fdec6e61e9a95a
Bello Oladipupo <bellooladipupo41@gmail.com>
2018-09-26 13:46:35 +0100

    Improve code readability

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,28 +4,13 @@ const { ApolloServer } = require('apollo-server-express');
 const serveStatic = require('serve-static');
 
 const schema = require('../graphql/schema');
-const getContext = require('../graphql/lib/getContext');
+// const getContext = require('../graphql/lib/getContext');
 
 // Setup Route Bindings
 module.exports = (app) => {
-  // if (process.env.NODE_ENV === 'development') app.use(require('cors')())
-
-  // Register API middleware
-  // app.use(
-  //   '/graphql',
-  //   bodyParser.json(),
-  //   graphqlExpress(() => ({ schema, context: getContext() })),
-  // );
-
-  // routes for testing in development
-  // if (process.env.NODE_ENV === 'development') {
-  //   app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-  // }
-
+  // const server = new ApolloServer({ schema, context: getContext() });
   const server = new ApolloServer({ schema });
-  server.applyMiddleware({ app, context: getContext() });
-  // console.log(server);
-
+  server.applyMiddleware({ app });
 
   // Views
   app.get('/admin', (req, res) => { res.redirect('/keystone'); });
