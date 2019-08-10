c8383be97af05635164ffede4f14504b41486290
Adetona Sunday <papasunnee@gmail.com>
2019-08-10 15:51:05 +0100

    contactUpdate

--- components/Contact/ContactText.js
+++ components/Contact/ContactText.js
@@ -14,33 +14,53 @@ export default () => {
             </Col>
           </Row>
           <div>
-            <Row>
+            <Row className="my-5">
               <Col md={6}>
-                <div style={{ width: "90%" }}>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Incidunt illo amet similique repudiandae, dolorem maxime
-                    reiciendis aspernatur nesciunt mollitia corrupti ab, fugiat
-                    illum est cumque, non placeat tenetur distinctio doloribus!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Incidunt illo amet similique repudiandae, dolorem maxime
-                    reiciendis aspernatur nesciunt mollitia corrupti ab, fugiat
-                    illum est cumque, non placeat tenetur distinctio doloribus!
-                  </p>
+                <div className="formWrapper">
+                  <form action="" className="contactForm">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="contact_number"
+                      placeholder="Contact Number"
+                    />
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email_address"
+                      placeholder="Email Address"
+                    />
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="address"
+                      placeholder="Address"
+                    />
+                  </form>
                 </div>
               </Col>
               <Col md={6}>
-                <div style={{ width: "90%" }}>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Incidunt illo amet similique repudiandae, dolorem maxime
-                    reiciendis aspernatur nesciunt mollitia corrupti ab, fugiat
-                    illum est cumque, non placeat tenetur distinctio doloribus!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Incidunt illo amet similique repudiandae, dolorem maxime
-                    reiciendis aspernatur nesciunt mollitia corrupti ab, fugiat
-                    illum est cumque, non placeat tenetur distinctio doloribus!
-                  </p>
+                <div className="contactDetails">
+                  <Row>
+                    <Col md={4}>
+                      <img
+                        className="img-fluid"
+                        src="/static/images/contact/contact.jpg"
+                      />
+                    </Col>
+                    <Col md={8}>
+                      <div className="contactList">
+                        <div>Contact Number</div>
+                        <div>Email Address</div>
+                        <div>Address</div>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md={12}>
+                      <img />
+                    </Col>
+                  </Row>
                 </div>
               </Col>
             </Row>
@@ -49,6 +69,43 @@ export default () => {
       </Container>
       <style jsx>
         {`
+          .contactList {
+            display: flex;
+            flex-direction: column;
+          }
+          .contactList div {
+            margin-bottom: 30px;
+          }
+
+          .contactDetails {
+            background-color: #ffda44;
+            padding: 50px;
+          }
+          .contactDetails div {
+            font-weight: bold;
+          }
+          .formWrapper {
+            max-width: 80%;
+            margin: auto;
+            border-top: 40px solid #0069d1;
+            padding-top: 20px;
+          }
+          .contactForm .form-control {
+            margin-bottom: 20px;
+            border-radius: 0px;
+            border: none;
+            border-bottom: 1px solid #ffda44;
+            outline: none;
+            box-shadow: none;
+          }
+          .contactForm .form-control::placeholder {
+            color: #0069d1;
+            font-weight: bold;
+          }
+          .contactForm .form-control:focus::placeholder {
+            color: #0069d1;
+            font-weight: normal;
+          }
           .aboutTitle {
             padding: 0 50px;
             margin: 40px 0px;
--- pages/locations.js
+++ pages/locations.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import Banner from "../components/Header/Banner";
 import Layout from "../components/Layout";
-import Title from "../components/About/Title";
-import Ready from "../components/Footer/Ready";
 import Location from "../components/Locations/location";
 
 export default class extends Component {
@@ -11,7 +9,6 @@ export default class extends Component {
     return (
       <Layout>
         <Banner bgImage="locations" />
-        <Title title="LOCATIONS" />
         <Location />
       </Layout>
     );
