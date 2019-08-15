addc25de267ec6269c6e161b7465e8f019ed1acb
Adetona Sunday <papasunnee@gmail.com>
2019-08-15 19:28:56 +0100

    commit

--- components/University/index.js
+++ components/University/index.js
@@ -1,18 +1,42 @@
+import React, { Fragment, useState, useEffect } from "react";
 import { Row, Col } from "reactstrap";
 import "./university.scss";
 
 export default () => {
+  const [state, setState] = useState(1);
+  const input = 0;
+  useEffect(() => {
+    changeImage();
+  }, [input]);
+  const changeImage = () => {
+    setInterval(() => {
+      setState(prev => {
+        let increment = ++prev;
+        if (increment == 4) return 1;
+        else return increment;
+      });
+    }, 5000);
+  };
   return (
-    <Row>
-      <Col sm={{ size: 0 }} md={4}>
-        <div className="university__student" />
-      </Col>
-      <Col md={8}>
-        <div className="university__main">
-          <img className="img-fluid" src="/static/images/footer/bmu.png" />
-          <h2>STARTING SOON</h2>
-        </div>
-      </Col>
-    </Row>
+    <Fragment>
+      <Row>
+        <Col sm={{ size: 0 }} md={4}>
+          <div className="university__student" />
+        </Col>
+        <Col md={8}>
+          <div className="university__main">
+            <img className="img-fluid" src="/static/images/footer/bmu.png" />
+            <h2>STARTING SOON</h2>
+          </div>
+        </Col>
+      </Row>
+      <style jsx>{`
+        .university__student {
+          min-height: 100vh;
+          background: url("/static/images/university/${state}.png") no-repeat -50px
+            0;
+        }
+      `}</style>
+    </Fragment>
   );
 };
