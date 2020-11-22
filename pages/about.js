8e5817dc7eac48790184461a3668afcedda3be01
Adetona Sunday <papasunnee@gmail.com>
2020-11-22 13:38:46 +0100

    update management row

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,7 @@ import Banner from "../components/Header/Banner";
 import Layout from "../components/Layout";
 import AboutText from "../components/About/AboutText";
 import Global from "../components/Global/Global";
+import ManagementRow from "../components/Management/managementRow";
 
 export default class extends Component {
   state = {};
@@ -11,7 +12,7 @@ export default class extends Component {
       <Layout>
         <Banner bgImage="about" min />
         <AboutText title="ABOUT US" />
-        <Global title="Global Management Team" />
+        <ManagementRow title="Management Team" />
       </Layout>
     );
   }
